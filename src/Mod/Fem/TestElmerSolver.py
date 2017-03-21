a9b40f12725a38bcfe4345010694d68a91c1106d
Bernd Hahnebach <bernd@bimstatik.org>
2017-03-21 14:01:55 +0100

    Add new solver object for Elmer integration
    First commit for the Elmer Itegration GSoC project. Adds the solver
    document object and view provider with no functionallity so far.

---
+++ src/Mod/Fem/TestElmerSolver.py
@@ -0,0 +1,89 @@
+# ***************************************************************************
+# *                                                                         *
+# *   Copyright (c) 2016 - Bernd Hahnebach <bernd@bimstatik.org>            *
+# *                                                                         *
+# *   This program is free software; you can redistribute it and/or modify  *
+# *   it under the terms of the GNU Lesser General Public License (LGPL)    *
+# *   as published by the Free Software Foundation; either version 2 of     *
+# *   the License, or (at your option) any later version.                   *
+# *   for detail see the LICENCE text file.                                 *
+# *                                                                         *
+# *   This program is distributed in the hope that it will be useful,       *
+# *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+# *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+# *   GNU Library General Public License for more details.                  *
+# *                                                                         *
+# *   You should have received a copy of the GNU Library General Public     *
+# *   License along with this program; if not, write to the Free Software   *
+# *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  *
+# *   USA                                                                   *
+# *                                                                         *
+# ***************************************************************************
+
+__title__="Elmer Solver Object"
+__author__ = "Markus Hovorka"
+__url__ = "http://www.freecadweb.org"
+
+
+import unittest
+import tempfile
+
+import FreeCAD as App
+import ObjectsFem
+import Fem
+
+if App.GuiUp:
+    import FreeCADGui as Gui
+    import FemGui
+
+
+def Gui():
+    classes = []
+    return _loadFromClasses(classes)
+
+
+def Cmd():
+    classes = [TestSolverObject]
+    return _loadFromClasses(classes)
+
+
+def All():
+    allSuite = unittest.TestSuite()
+    allSuite.addTest(Cmd())
+    if App.GuiUp:
+        allSuite.addTest(Gui())
+    return allSuite
+
+
+def _loadFromClasses(classes):
+    allSuite = unittest.TestSuite()
+    for c in classes:
+        oneSuite = unittest.defaultTestLoader.loadTestsFromTestCase(c)
+        allSuite.addTest(oneSuite)
+    return allSuite
+
+
+class TestSolverObject(unittest.TestCase):
+
+    def setUp(self):
+        self.doc = App.newDocument()
+        self.analysis_name = "test_analysis"
+        self.solver_name = "test_solver"
+        self.analysis = ObjectsFem.makeAnalysis(self.analysis_name)
+        self.solver = ObjectsFem.makeSolverElmer(self.solver_name)
+        self.analysis.Member += [self.solver]
+
+    def tearDown(self):
+        App.closeDocument(self.doc.Name)
+
+    def testSimpleSaveRestore(self):
+        with tempfile.TemporaryFile() as docFile:
+            self.doc.saveAs(docFile.name)
+            App.closeDocument(self.doc.Name)
+            self.doc = App.open(docFile.name)
+
+        self.analysis = self.doc.getObject(self.analysis_name)
+        self.solver = self.doc.getObject(self.solver_name)
+        self.assertIsNotNone(self.analysis)
+        self.assertIsNotNone(self.solver)
+        self.assertEqual(self.solver.SolverType, "FemSolverElmer")

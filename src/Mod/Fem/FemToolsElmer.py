e78d9079727a69aed9855535aa959aba19b2faa9
Markus Hovorka <m.hovorka@live.de>
2017-06-27 08:53:46 +0200

    FEM: elmer, fix unsupported constraints check 2

--- src/Mod/Fem/FemToolsElmer.py
+++ src/Mod/Fem/FemToolsElmer.py
@@ -79,6 +79,7 @@ _SPECIAL_MEMBERS = [
         ("App::TextDocument",),
         ("Fem::FemResultObjectPython",),
         ("Fem::FemResultObject",),
+        ("Fem::FemMeshShapeNetgenObject",),
 ]
 
 

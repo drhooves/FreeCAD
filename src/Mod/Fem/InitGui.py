0f24e7e3e3dc389d462f208899d406f8bbf91991
Markus Hovorka <m.hovorka@live.de>
2017-06-27 15:37:32 +0200

    FEM: elmer, add open in paraview command
    Works only for simulations solved with elmer solver. Support for
    calculix will be avaliable as soon as it shares the code with elmer
    solver.

--- src/Mod/Fem/InitGui.py
+++ src/Mod/Fem/InitGui.py
@@ -65,6 +65,7 @@ class FemWorkbench (Workbench):
         import PyGui._CommandFemSolverCalculix
         import PyGui._CommandFemSolverControl
         import PyGui._CommandFemSolverElmer
+        import PyGui._CommandFemOpenParaview
         import PyGui._CommandFemSolverRun
         import PyGui._CommandFemSolverZ88
 

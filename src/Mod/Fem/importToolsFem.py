fa404b232e350cfddfa4606ecc28afb39ab6fed8
Bernd Hahnebach <bernd@bimstatik.org>
2017-07-07 07:46:44 +0200

    FEM: start fixing result and inp import of quadratic edges, 1DFlow seams different order

--- src/Mod/Fem/importToolsFem.py
+++ src/Mod/Fem/importToolsFem.py
@@ -113,17 +113,20 @@ def make_femmesh(mesh_data):
     m = mesh_data
     if ('Nodes' in m) and (len(m['Nodes']) > 0):
         print("Found: nodes")
-        if (('Seg2Elem' in m) or
-           ('Tria3Elem' in m) or
-           ('Tria6Elem' in m) or
-           ('Quad4Elem' in m) or
-           ('Quad8Elem' in m) or
-           ('Tetra4Elem' in m) or
-           ('Tetra10Elem' in m) or
-           ('Penta6Elem' in m) or
-           ('Penta15Elem' in m) or
-           ('Hexa8Elem' in m) or
-           ('Hexa20Elem' in m)):
+        if (
+                ('Seg2Elem' in m) or
+                ('Seg3Elem' in m) or
+                ('Tria3Elem' in m) or
+                ('Tria6Elem' in m) or
+                ('Quad4Elem' in m) or
+                ('Quad8Elem' in m) or
+                ('Tetra4Elem' in m) or
+                ('Tetra10Elem' in m) or
+                ('Penta6Elem' in m) or
+                ('Penta15Elem' in m) or
+                ('Hexa8Elem' in m) or
+                ('Hexa20Elem' in m)
+        ):
 
             nds = m['Nodes']
             print("Found: elements")
@@ -176,10 +179,14 @@ def make_femmesh(mesh_data):
             for i in elms_seg2:
                 e = elms_seg2[i]
                 mesh.addEdge(e[0], e[1])
+            elms_seg3 = m['Seg3Elem']
+            for i in elms_seg3:
+                e = elms_seg3[i]
+                mesh.addEdge(e[0], e[2], e[1])
             print("imported mesh: {} nodes, {} HEXA8, {} PENTA6, {} TETRA4, {} TETRA10, {} PENTA15".format(
                   len(nds), len(elms_hexa8), len(elms_penta6), len(elms_tetra4), len(elms_tetra10), len(elms_penta15)))
-            print("imported mesh: {} HEXA20, {} TRIA3, {} TRIA6, {} QUAD4, {} QUAD8, {} SEG2".format(
-                  len(elms_hexa20), len(elms_tria3), len(elms_tria6), len(elms_quad4), len(elms_quad8), len(elms_seg2)))
+            print("imported mesh: {} HEXA20, {} TRIA3, {} TRIA6, {} QUAD4, {} QUAD8, {} SEG2, {} SEG3".format(
+                  len(elms_hexa20), len(elms_tria3), len(elms_tria6), len(elms_quad4), len(elms_quad8), len(elms_seg2), len(elms_seg3)))
         else:
             FreeCAD.Console.PrintError("No Elements found!\n")
     else:
